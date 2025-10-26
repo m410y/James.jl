a5efecd67332b7154ca5dd72aa3138d0b48546c7
m410y <m410y@proton.me>
2025-10-26 23:11:09 +0700

    delete cringe

--- src/crystal.jl
+++
@@ -1,9 +0,0 @@
-struct Crystal{T}
-    UB::SMatrix{3,3,T,9}
-    p::PVec
-end
-
-const Miller{T<:Real} = SVector{3,T}
-
-miller(crystal::Crystal, q::PVec) = Miller(crystal.UB \ SA[q.e032, q.e013, q.e021])
-direction(crystal::Crystal, hkl::Miller) = direction(crystal.UB * hkl)
