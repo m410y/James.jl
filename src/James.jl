135399e95572491f7fcad1c899345d42f830c2f9
m410y <m410y@proton.me>
2025-10-26 23:25:06 +0700

    purification

--- src/James.jl
+++ src/James.jl
@@ -1,25 +1,5 @@
 module James
 
-using LinearAlgebra
-using Statistics
-using Dates
 
-using StaticArrays
-using Unitful
-using UnitfulEquivalences
 
-using LeastSquaresOptim
-
-using PGA3D
-using SFRM
-using P4P
-
-const runit = u"keV"
-const munit = u"mm"
-
-include("detector.jl")
-include("crystal.jl")
-include("model.jl")
-include("geometry.jl")
-
-end # module James
+end
